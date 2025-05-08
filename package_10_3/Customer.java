ce13810ed80bf610cac66f00697512f60a936ad5
Josh <joshualopezx@gmail.com>
2025-05-08 16:41:11 -0700

    Update Customer.java

--- package_10_3/Customer.java
+++ package_10_3/Customer.java
@@ -4,9 +4,9 @@ package package_10_3;
  *Customer class is a subclass of the Person superclass. Customer includes extra fields such as the idNumber and mailList variables. 
  *It also passes in the name, address, and number variables in its constructor to the Person constructor using the super keyword*/
 public class Customer extends Person{
-
+	
 	private String customerNumber;
-    private boolean receiveMail;
+	private boolean receiveMail;
 
     public Customer(String name, String address, String number, String idNumber, boolean receiveMail) {
         super(name, address, number); // Call the superclass (Person) constructor
